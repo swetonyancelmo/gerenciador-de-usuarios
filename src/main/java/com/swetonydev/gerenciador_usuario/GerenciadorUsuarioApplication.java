5e7d2edd0b6a3de906ab77cee90a0f5fc15e6a41
Swetony <swetonyancelmo@gmail.com>
2025-08-18 20:20:32 -0300

    project created

---
+++ src/main/java/com/swetonydev/gerenciador_usuario/GerenciadorUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.swetonydev.gerenciador_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorUsuarioApplication.class, args);
+	}
+
+}
